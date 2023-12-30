41ea2e38268745aea927590ccf297a2dd6916926
ErrorTek <jakub.baranski.33@gmail.com>
2023-12-30 08:31:46 +0000

    Dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.core:core-ktx:1.13.0-alpha02")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
+    implementation("com.google.android.material:material:1.12.0-alpha02")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0-rc02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-rc02")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0-alpha02")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0-alpha02")
 }
